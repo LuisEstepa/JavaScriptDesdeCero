187396fc8172724304a9244f803d9fe8efc4e308
LuisEstepa <116852447+LuisEstepa@users.noreply.github.com>
2024-02-26 22:21:24 -0500

    ciclos

---
+++ JavaScriptDesdeCero/clase-do-while.js
@@ -0,0 +1,14 @@
+/* 
+
+do {
+  cÃģdigo
+} while (condicion)
+
+*/
+
+let contador = 0;
+
+do {
+  console.log(contador);
+  contador++;
+} while (contador < 10);
---
+++ JavaScriptDesdeCero/clase-loop-for.js
@@ -0,0 +1,9 @@
+// for (variable; condicion; incremento){
+//     códogo a ejecutar
+// }
+
+let list = ["eat", "sleep", "code", "repeat"]
+
+for (let i = 0 ; i < list.length ; i ++ ){
+    console.log(`la variable i vale = ${i} y en esa posicion esta "${list[i]}"`)
+}
